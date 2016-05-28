95c9b4bd63a03e0d6f239e30e1adfea6a5a27d0b
ershad459 <Ershad@ershads-macbook-pro.local>
2016-05-28 21:01:16 +0430

    Initial Commit

---
+++ pokemon 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokemon 2
+//
+//  Created by Ershad Ilkhan on 5/28/16.
+//  Copyright © 2016 Ershad Ilkhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
